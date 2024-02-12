6d9a7655f1bd00a1485405d82666a2ac966decd7
Astra <abhaypratapsinghwd@gmail.com>
2024-02-13 04:21:43 +0530

    Express Server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
+// const userRoutes = require("./routes/user");
+
+// const CONNECTION_URL = process.env.CONNECTION_URL;
+
+// mongoose
+//   .connect(CONNECTION_URL)
+//   .then(() => {
+//     console.log("Connected to the Database Successfully");
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// app.use("/post", postRoutes);
+// app.use("/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Chat Server is Running.");
+});
+
+app.listen(5000, () => {
+  console.log("Server is Listening on Port 5000");
+});
