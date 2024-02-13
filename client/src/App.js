c66664a762841c53b778438d3ad162bf5bca30bc
Astra <abhaypratapsinghwd@gmail.com>
2024-02-13 15:16:32 +0530

    Refactor routing logic in App component for better readability and consolidation

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,33 @@ import ChatArea from "./Components/ChatArea";
 import OnlineUsers from "./Components/OnlineUsers";
 import OnlineGroups from "./Components/OnlineGroups";
 import CreateGroup from "./Components/CreateGroup";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const user = useSelector((state) => state.user);
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="app" element={<MainContainer />}>
-          <Route path="welcome" element={<Welcome />} />
-          <Route path="chat" element={<ChatArea />} />
-          <Route path="users" element={<OnlineUsers />} />
-          <Route path="groups" element={<OnlineGroups />} />
-          <Route path="create-group" element={<CreateGroup />} />
-        </Route>
+        {/* Routes for authenticated users */}
+        {user && (
+          <Route path="/" element={<Navigate to="/app/welcome" replace />} />
+        )}
+        {user && (
+          <Route path="app" element={<MainContainer />}>
+            <Route path="welcome" element={<Welcome />} />
+            <Route path="chat" element={<ChatArea />} />
+            <Route path="users" element={<OnlineUsers />} />
+            <Route path="groups" element={<OnlineGroups />} />
+            <Route path="create-group" element={<CreateGroup />} />
+          </Route>
+        )}
+
+        {/* Route for non-authenticated users */}
+        {!user && <Route path="/" element={<Auth />} />}
+
+        {/* Redirect to login for unauthorized sub-routes */}
+        {!user && <Route path="app" element={<Navigate to="/" replace />} />}
       </Routes>
     </div>
   );
