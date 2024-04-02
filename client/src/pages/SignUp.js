f9ba9f08fb8475d34e65bbb8657a0e995817e90a
Astra <abhaypratapsinghwd@gmail.com>
2024-04-02 17:26:19 +0530

    Added Formik and Yup Validation to Sign Up

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,7 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import { register } from "../Redux/actions/userAction";
+import { Formik } from "formik";
 
 const SignUp = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const initialSignUpValues = {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const signUpValidationSchema = Yup.object().shape({
+    username: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string().required("Required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password")], "Passwords must match")
+      .required("Required"),
+  });
+
+  const onSubmit = (values, { setSubmitting }) => {
+    setSubmitting(true);
+    console.log("isRegister values is ", values);
+    // dispatch(
+    //   register(values.username, values.email, values.password, navigate)
+    // );
+  };
+
   return (
     <div className="flex bg-black w-full h-screen font-mono">
       <div className="flex-1 flex items-center justify-center w-full h-screen flex-col">
@@ -10,31 +41,78 @@ const SignUp = () => {
             Create Your Account
           </h1>
         </div>
-        <form className="w-4/5 flex flex-col gap-3">
-          <input
-            className="w-full h-10 bg-transparent text-white font-mono outline outline-0 focus:outline-0 border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] focus:border-violet-400"
-            placeholder="Username"
-            type="text"
-          />
-          <input
-            className="w-full h-10 bg-transparent text-white font-mono outline outline-0 focus:outline-0 border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] focus:border-violet-400"
-            placeholder="Email"
-            type="email"
-          />
-          <input
-            className="w-full h-10 bg-transparent text-white font-mono outline outline-0 focus:outline-0 border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] focus:border-violet-400"
-            placeholder="Password"
-            type="password"
-          />
-          <input
-            className="w-full h-10 bg-transparent text-white font-mono outline outline-0 focus:outline-0 border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] focus:border-violet-400"
-            placeholder="Confirm Password"
-            type="password"
-          />
-          <button class="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 border border-violet-700 rounded w-full mt-3">
-            Sign Up
-          </button>
-        </form>
+        <Formik
+          initialValues={initialSignUpValues}
+          validationSchema={signUpValidationSchema}
+          onSubmit={onSubmit}
+        >
+          {(props) => {
+            const {
+              values,
+              touched,
+              errors,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+            } = props;
+            console.log("values is ", values);
+            console.log("touched is ", touched);
+            console.log("errors is ", errors);
+            return (
+              <form
+                className="w-4/5 flex flex-col gap-3"
+                onSubmit={handleSubmit}
+              >
+                <input
+                  className="w-full h-10 bg-transparent text-white font-mono outline outline-0 focus:outline-0 border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] focus:border-violet-400"
+                  placeholder="Username"
+                  type="text"
+                  id="username"
+                  autoComplete="off"
+                  value={values.username}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <input
+                  className="w-full h-10 bg-transparent text-white font-mono outline outline-0 focus:outline-0 border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] focus:border-violet-400"
+                  placeholder="Email"
+                  type="email"
+                  id="email"
+                  autoComplete="off"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <input
+                  className="w-full h-10 bg-transparent text-white font-mono outline outline-0 focus:outline-0 border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] focus:border-violet-400"
+                  placeholder="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="off"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <input
+                  className="w-full h-10 bg-transparent text-white font-mono outline outline-0 focus:outline-0 border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] focus:border-violet-400"
+                  placeholder="Confirm Password"
+                  type="password"
+                  id="confirmPassword"
+                  autoComplete="off"
+                  value={values.confirmPassword}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <button
+                  className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 border border-violet-700 rounded w-full mt-3"
+                  type="submit"
+                >
+                  Sign Up
+                </button>
+              </form>
+            );
+          }}
+        </Formik>
         <div className="w-4/5 text-right mt-3">
           <Link to="/log-in">
             <p className="text-base hover:text-violet-400 cursor-pointer hover:underline text-white">
