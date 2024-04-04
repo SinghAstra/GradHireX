edfcd66e09b1ec129033a02e6b84bc9de756ec0b
Astra <abhaypratapsinghwd@gmail.com>
2024-04-04 08:24:12 +0530

    AddUserItem

--- client/src/Components/Groups/AddUserItem.js
+++ client/src/Components/Groups/AddUserItem.js
@@ -3,11 +3,27 @@ import UserAvatar from "../Conversations/UserAvatar";
 
 const AddUserItem = ({ user }) => {
   return (
-    <div className="flex w-full font-mono gap-2 m-1 p-1 rounded-md  hover:bg-violet-300 hover:cursor-pointer">
-      <UserAvatar chatName={user.name} />
-      <div className="flex-1 flex items-center justify-start">
-        <p className="text-2xl text-white">{user.name}</p>
-      </div>
+    // <div className="flex w-full font-mono gap-2 m-1 p-1 rounded-md  hover:bg-violet-300 hover:cursor-pointer form-control">
+    //   <div className="label">
+    // <UserAvatar chatName={user.name} />
+    //     <div className="flex-1 flex items-center justify-start ">
+    //       <p className="text-2xl text-white label-text">{user.name}</p>
+    //     </div>
+    //     <input
+    //       type="checkbox"
+    //       checked="checked"
+    //       class="checkbox checkbox-primary"
+    //     />
+    //   </div>
+    // </div>
+    <div className=" w-full font-mono  m-1 p-1 rounded-md  hover:bg-violet-300 form-control">
+      <label className="label cursor-pointer flex gap-2">
+        <UserAvatar chatName={user.name} />
+        <span className="label-text flex-1 flex justify-start items-center">
+          {user.name}
+        </span>
+        <input type="checkbox" className="checkbox checkbox-primary" />
+      </label>
     </div>
   );
 };
