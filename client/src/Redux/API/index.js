a7012264e553ebdcd5e86bc3cfad4dd0a7757261
Astra <abhaypratapsinghwd@gmail.com>
2024-04-04 12:35:46 +0530

    Deployment Bug Fix

--- client/src/Redux/API/index.js
+++ client/src/Redux/API/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/api/";
+const API_URL = "https://chat-webapp-api.onrender.com";
 
 const API = axios.create({ baseURL: API_URL });
 
