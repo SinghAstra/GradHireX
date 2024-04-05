f87b80439308a3a4e7fcd10187e7e4836b3a223d
Astra <abhaypratapsinghwd@gmail.com>
2024-04-05 07:25:04 +0530

    updated api url

--- client/src/Redux/API/index.js
+++ client/src/Redux/API/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const API_URL = "https://chat-webapp-api.onrender.com/api/";
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://chat-webapp-api.onrender.com/api/";
+// const API_URL = "http://localhost:5000/api";
 
 const API = axios.create({ baseURL: API_URL });
 
