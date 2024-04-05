6cf2bd404555d0851a903f0eee4304f70cd4f19e
Astra <abhaypratapsinghwd@gmail.com>
2024-04-05 08:36:48 +0530

    Bug Fix

--- server/index.js
+++ server/index.js
@@ -33,6 +33,6 @@ app.get("/", (req, res) => {
   res.send("Chat Server is Running.");
 });
 
-server.listen(5000, () => {
+app.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
