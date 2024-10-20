182d2928d646d74a7ccc9828e0d414599f0ad369
Astra <abhaypratapsinghwd@gmail.com>
2024-10-20 23:05:12 +0530

    Auth Routes

--- auth.config.ts
+++ auth.config.ts
@@ -1,69 +1,74 @@
+import { prisma } from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { compare } from "bcrypt";
+import { User } from "@prisma/client";
+import bcrypt from "bcrypt";
 import { NextAuthConfig } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { prisma } from "./lib/db";
-
-const providers = [
-  CredentialsProvider({
-    name: "Credentials",
-    credentials: {
-      email: { label: "Email", type: "text" },
-      password: { label: "Password", type: "password" },
-    },
-    async authorize(credentials) {
-      if (!credentials?.email || !credentials?.password) {
-        return null;
-      }
-      const user = await prisma.user.findUnique({
-        where: { email: credentials.email as string },
-      });
-      if (!user) {
-        return null;
-      }
-      const isPasswordValid = await compare(
-        credentials.password as string,
-        user.password
-      );
-      if (!isPasswordValid) {
-        return null;
-      }
-      return {
-        id: user.id,
-        email: user.email,
-        name: user.name,
-        role: user.role,
-      };
-    },
-  }),
-];
 
 export const authOptions: NextAuthConfig = {
-  adapter: PrismaAdapter(prisma),
-  providers,
+  adapter: PrismaAdapter(prisma) as any,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const isPasswordValid = await bcrypt.compare(
+          credentials.password as string,
+          user.password
+        );
+
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          role: user.role,
+        };
+      },
+    }),
+  ],
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
         token.role = user.role;
+        token.id = user.id;
       }
       return token;
     },
     async session({ session, token }) {
       if (session?.user) {
-        session.user.role = token.role as string;
+        session.user.role = token.role as User["role"];
+        session.user.id = token.id as string;
       }
       return session;
     },
   },
   pages: {
     signIn: "/auth/sign-in",
-    signOut: "/auth/sign-out",
-    error: "/auth/error",
-    verifyRequest: "/auth/verify-request",
-    newUser: "/auth/new-user",
+    newUser: "/auth/sign-up",
   },
-  secret: process.env.NEXT_AUTH_SECRET,
   session: {
     strategy: "jwt",
   },
+  secret: process.env.NEXT_AUTH_SECRET,
 };
